0b12d24d3866ac1825d562b4eea3bf2e9c59c07d
Axel Rietschin <axelriet@gmail.com>
2026-05-02 03:06:13 -0700

    Completed MiniCache sample.

--- samples/Caching/MiniCache/MiniCache.cpp
+++ samples/Caching/MiniCache/MiniCache.cpp
@@ -29,28 +29,40 @@ Environment:
 
 #include <Windows.h>
 
-#include <io.h>
-#include <fcntl.h>
-#include <process.h>
+#include <format>
 
 #define USE_PRECISE_BUT_SLOWER_TIMESTAMPS
 
+#include <api-lwmq-time.h>
 #include <api-lwmq-cache.h>
+
 #include <api-lwmq-samples-common.h>
 
-#pragma warning(disable: 26481) // Don't use pointer arithmetic.
+#define ONE_MILLION (1'000'000)
+
+CHAR PayloadText1024[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                         "Suspendisse maximus vel odio quis ultrices. Sed bibendum "
+                         "fringilla sem sit amet porta. Donec tristique tortor cursus, "
+                         "ultrices turpis a, finibus tellus. Donec at velit fringilla, "
+                         "volutpat nulla ut, ullamcorper risus. Maecenas non tempor "
+                         "sapien, feugiat consectetur elit. Nunc sed nibh sed velit "
+                         "molestie hendrerit. Sed sollicitudin, ligula vel porta fermentum, "
+                         "tortor enim tempor leo, vitae convallis nulla nunc ac ante. Nam "
+                         "molestie leo et urna posuere porttitor. Morbi non tincidunt purus, "
+                         "quis ultricies neque. Cras sed varius lectus. Morbi dictum risus "
+                         "ac neque finibus, vel iaculis odio rhoncus. Vivamus euismod neque "
+                         "massa, at efficitur sem sollicitudin id. Integer iaculis odio sed "
+                         "diam dignissim, a pulvinar purus scelerisque. Mauris a interdum "
+                         "arcu, vitae sodales arcu. Integer ac purus venenatis odio rhoncus "
+                         "interdum. Cras pretium maximus dignissim. Nulla sed diam ut risus "
+                         "tristique varius at in augue. Mauris nec lectus a est sollicitudin "
+                         "imperdiet vel.";
 
 int main()
 {
-    printf("MiniCache 1.0\n");
+    std::locale::global(std::locale("en_US.UTF-8"));
 
-#ifdef USE_PRECISE_BUT_SLOWER_TIMESTAMPS
-
-    g_TimingAdjustmentNs = ComputeTimingOverhead();
-
-    printf("Using precise timestamps with a time adjustment of %.0fns.\n", g_TimingAdjustmentNs);
-
-#endif
+    printf("MiniCache 1.0\nSingle cache, 1KB entries.\n1 million slots, 1 million inserts, 1 million retrieval.\n");
 
     LMQ_KEY Key{};
     LMQ_CACHE Cache{};
@@ -70,7 +82,7 @@ int main()
         sizeof(LMQ_CACHEPARAMETERS),
         LMQ_CACHETYPE_DATA_CACHE,
         LMQ_CACHEFLAGS_NONE,
-        1'000'000
+        ONE_MILLION
     };
 
     CHECK(LmqCreateCache(&Parameters,
@@ -87,55 +99,91 @@ int main()
     // 
     //
 
-    for (DWORD Index = 0; Index < 2'000'000; Index++)
+    printf("\nInserting 1 million x 1KB entries in the LRU cache.\n");
+
+    UINT64 StartNs = LmqGetTickCountNs();
+
+    for (DWORD Index = 0; Index < ONE_MILLION; Index++)
     {
         //
-        // We stuff the Index in the key. That makes 2 million
+        // We stuff the Index in the key. That makes 1 million
         // unique key although they only differ slightly.
         //
 
-    }   Key.DWord0 = Index;
+        Key.DWord0 = Index;
 
-            //
-            // 1KB is below VA limit (currently 16KB)
-            //
+        CHECK(LmqAddCacheEntry(Cache,
+                               &Key,
+                               &PayloadText1024[0],
+                               sizeof(PayloadText1024),
+                               LMQ_CACHEENTRY_NO_ADDITIONAL_ENTROPY,
+                               LMQ_CACHEENTRY_FLAGS_NONE,
+                               0.0f));
+    }
 
-            Assert::IsTrue(SUCCEEDED(LmqAddCacheEntry(Cache,
-                                                      &Key,
-                                                      (PVOID)LipsumText1K,
-                                                      sizeof(LipsumText1K),
-                                                      LMQ_CACHEENTRY_NO_ADDITIONAL_ENTROPY,
-                                                      LMQ_CACHEENTRY_FLAGS_NONE,
-                                                      0.0f)));
+    UINT64 ElapsedNs = LmqTimeElapsedNsSince(StartNs);
 
-            SIZE_T DataSize{};
+    double Throughput = ((double)ONE_MILLION / ElapsedNs * 1'000'000'000.0);
 
-            Assert::IsTrue(DataSize == sizeof(LipsumText1K));
+    printf("Elapsed: %s [ms] @ %s insertion/sec (1KB items)\n\n",
+           std::format("{:.0Lf}", ElapsedNs / 1'000'000.0).c_str(),
+           std::format("{:.0Lf}", Throughput).c_str());
 
-            BYTE Text[sizeof(LipsumText1K)];
+    //
+    // Retrieve all entries.
+    //
 
-            Assert::IsTrue(SUCCEEDED(LmqRetrieveCacheEntry(Cache,
-                                                           &Key,
-                                                           Text,
-                                                           &DataSize,
-                                                           LMQ_CACHEENTRY_NO_ADDITIONAL_ENTROPY,
-                                                           nullptr)));
+    printf("Retrieving 1 million x 1KB entries maintaining LRU property.\n");
 
-            Assert::IsTrue(strcmp((PSTR)Text, LipsumText1K) == 0);
+    StartNs = LmqGetTickCountNs();
 
-            Assert::IsTrue(SUCCEEDED(LmqRemoveCacheEntry(Cache,
-                                                         &Key)));
+    for (DWORD Index = 0; Index < ONE_MILLION; Index++)
+    {
+        Key.DWord0 = Index;
 
-            HRESULT hr = LmqDestroyCache(&Cache);
+        SIZE_T DataSize{ sizeof(PayloadText1024) };
+        BYTE RetrievedText[sizeof(PayloadText1024)];
 
-            Assert::IsTrue(SUCCEEDED(hr));
+        CHECK(LmqRetrieveCacheEntry(Cache,
+                                    &Key,
+                                    &RetrievedText[0],
+                                    &DataSize,
+                                    LMQ_CACHEENTRY_NO_ADDITIONAL_ENTROPY,
+                                    nullptr));
+    }
 
+    ElapsedNs = LmqTimeElapsedNsSince(StartNs);
 
+    Throughput = ((double)ONE_MILLION / ElapsedNs * 1'000'000'000.0);
 
+    printf("Elapsed: %s [ms] @ %s retrieval/sec (1KB items)\n",
+            std::format("{:.0Lf}", ElapsedNs / 1'000'000.0).c_str(),
+            std::format("{:.0Lf}", Throughput).c_str());
 
+    //
+    // Clear the cache.
+    //
 
+    printf("\nClearing the cache.\n");
 
+    StartNs = LmqGetTickCountNs();
 
+    CHECK(LmqClearCache(Cache,
+                        FALSE));
+
+    ElapsedNs = LmqTimeElapsedNsSince(StartNs);
+
+    Throughput = ((double)ONE_MILLION / ElapsedNs * 1'000'000'000.0);
+
+    printf("Elapsed: %s [ms] @ %s entries/sec (frees all items)\n",
+            std::format("{:.0Lf}", ElapsedNs / 1'000'000.0).c_str(),
+            std::format("{:.0Lf}", Throughput).c_str());
+
+    //
+    // Done, destroy cache.
+    //
+
+    CHECK(LmqDestroyCache(&Cache));
 
     return 0;
 }
